6086c0350205190b681a8d25a6d9a7db5c581e15
Matheusxd676 <matheussgameer@gmail.com>
2023-05-25 15:27:11 -0300

    Update pterodactyl-installer.sh

--- pterodactyl-installer.sh
+++ pterodactyl-installer.sh
@@ -1,95 +1,30 @@
 #!/bin/bash
 
-# Atualize o sistema operacional
+# Atualizar o sistema
 sudo apt update
 sudo apt upgrade -y
 
-# Instale as dependências necessárias
-sudo apt install -y curl software-properties-common zip unzip
+# Adicione o comando "add-apt-repository" 
+apt -y install software-properties-common curl apt-transport-https ca-certificates gnupg
 
-# Adicione o repositório do PHP 8.2
-sudo add-apt-repository -y ppa:ondrej/php
-sudo apt update
+# Adicionar repositórios adicionais para PHP, Redis e MariaDB
+LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
 
-# Instale o PHP 8.2 e extensões necessárias
-sudo apt install -y php8.2 php8.2-{cli,gd,mysql,pdo,mbstring,tokenizer,bcmath,xml,fpm,curl,zip}
+# Adicionar repositório APT oficial do Redis
+curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
 
-# Instale o MySQL Server
-sudo apt install -y mysql-server
+# O script de configuração do repositório MariaDB pode ser ignorado no Ubuntu 22.04
+curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
 
-# Configurar o MySQL
-sudo mysql -u root <<MYSQL_SCRIPT
-CREATE DATABASE panel;
-CREATE USER 'pterodactyl'@'127.0.0.1' IDENTIFIED BY 'FWEIDNWOIA3@#@10c';
-GRANT ALL PRIVILEGES ON panel.* TO 'pterodactyl'@'127.0.0.1';
-FLUSH PRIVILEGES;
-MYSQL_SCRIPT
+# Atualizar os repositórios
+apt update
 
-# Instale o Redis Server
-sudo apt install -y redis-server
+# Adicione o repositório universe se você estiver no Ubuntu 18.04
+apt-add-repository universe
 
-# Configure o Redis
-sudo sed -i "s/^supervised no/supervised systemd/" /etc/redis/redis.conf
-sudo sed -i "s/^# requirepass foobared/requirepass MDIWANDI9231#@1/" /etc/redis/redis.conf
-sudo systemctl restart redis.service
+# Instalar dependências
+apt -y install php8.1 php8.1-{common,cli,gd,mysql,mbstring,bcmath,xml,fpm,curl,zip} mariadb-server nginx tar unzip git redis-server
 
-# Baixe o painel Pterodactyl
-curl -Lo panel.tar.gz https://github.com/pterodactyl/panel/releases/download/v1.11.3/panel.tar.gz
-mkdir -p /var/www/pterodactyl
-tar --strip-components=1 -xzvf panel.tar.gz -C /var/www/pterodactyl
-cd /var/www/pterodactyl
+# Instalar composer
 
-# Instale o Composer e as dependências do Pterodactyl
-curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
-cp .env.example .env
-composer install --no-dev --optimize-autoloader
-
-# Configure o painel Pterodactyl
-php artisan key:generate --force
-php artisan p:environment:setup --n --url=\$SERVER_IP --timezone=America/Sao_Paulo --cache=redis --session=database --queue=redis --redis-host=127.0.0.1 --redis-pass=MDIWANDI9231#@1
-php artisan p:environment:database --n --host=127.0.0.1 --port=3306 --database=panel --username=pterodactyl --password=FWEIDNWOIA3@#@10c
-php artisan migrate --seed --force
-php artisan p:user:make --n --admin=1 --email=admin@admin.com --username=admin --name-first=nome1 --name-last=nome2 --password=admin
-
-## Continuação do script
-
-```bash
-# Configure o NGINX
-cat <<EOF > /etc/nginx/sites-available/pterodactyl.conf
-server_tokens off;
-server_name _;
-root /var/www/pterodactyl/public;
-location / {
-    try_files $uri $uri/ /index.php?$query_string;
-}
-location ~ \.php$ {
-    try_files $uri =404;
-    fastcgi_split_path_info ^(.+\.php)(/.+)$;
-    fastcgi_pass unix:/run/php/php8.2-fpm.sock;
-    fastcgi_index index.php;
-    include fastcgi_params;
-    fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-    fastcgi_param HTTP_PROXY "";
-}
-location ~ /\.ht {
-    deny all;
-}
-EOF
-
-ln -s /etc/nginx/sites-available/pterodactyl.conf /etc/nginx/sites-enabled/
-nginx -t
-systemctl restart nginx
-
-# Configure as permissões do Pterodactyl
-chown -R www-data:www-data /var/www/pterodactyl/*
-chmod -R 755 /var/www/pterodactyl/storage /var/www/pterodactyl/bootstrap/cache/
-
-# Adicione o cronjob do Pterodactyl
-echo "* * * * * php /var/www/pterodactyl/artisan schedule:run >> /dev/null 2>&1" | sudo crontab -u www-data -
-
-# Reinicie os serviços
-systemctl restart nginx
-systemctl restart php8.2-fpm
-
-# O painel Pterodactyl agora deve estar disponível e configurado!
-echo "A instalação do painel Pterodactyl foi concluída. Por favor, acesse o painel através do seu navegador."
